8602b2922f92a53fd893741e4e62b17284185352
Eric <57109017+ebarrio19@users.noreply.github.com>
2020-02-26 01:57:09 -0800

    Beginning implementation of Office panel
    Clicking on a sub-button in a category will now load its corresponding page in the panel box. Currently only the "Center Information" page is done since I was just trying to figure out the most effecient way I could set it up, but it should be easy to do the rest now.

--- WebApp/office.js
+++ WebApp/office.js
@@ -1,6 +1,13 @@
-function changeText(text) {
-
-    document.getElementById('officetitle').innerHTML = text;
-    document.getElementById('officebody').innerHTML = text + ' panel here';
-
+function start() {  
+    $(".box-content").click(function(){
+        var id = $(this).attr('id');
+        display(id);
+    });
 }
+
+function display(id) {
+    document.getElementById('officetitle').innerHTML = document.getElementById(id).innerHTML;
+    $("#officebody").load("officepages/" + id + ".html");
+}
+
+$(document).ready(start);
