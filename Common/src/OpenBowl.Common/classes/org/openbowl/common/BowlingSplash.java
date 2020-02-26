958176028c7743afbdbe4240be2ceb9594991110
patrick <patrick@enburg.info>
2020-02-26 07:24:50 -0800

    Added an enum for splash / exciters

---
+++ Common/src/OpenBowl.Common/classes/org/openbowl/common/BowlingSplash.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.openbowl.common;
+
+/**
+ *
+ * @author Open Bowl <http://www.openbowlscoring.org/>
+ */
+public enum BowlingSplash {
+    Foul,
+    Gutter,
+    Spare,
+    Split,
+    Split_Success,
+    SlowBall,
+    Strike,
+    Strike_2,
+    Strike_3,
+    Strike_4,
+    Strike_5,
+    Strike_6,
+    Strike_7,
+    Strike_8,
+    Strike_9,
+    Strike_10,
+    Strike_11,
+    PerfectGame,
+    Turkey,
+    NONE,
+}
