280c27f81971b7047bd4ab9c57e7908ed262bfdf
patrick <patrick@enburg.info>
2020-03-05 06:46:39 -0800

    Refactored to use common http handler

--- Display/src/OpenBowl.Display/classes/org/openbowl/display/BowlingGameDisplayHandler.java
+++ Display/src/OpenBowl.Display/classes/org/openbowl/display/BowlingGameDisplayHandler.java
@@ -5,90 +5,29 @@
  */
 package org.openbowl.display;
 
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
-import java.io.IOException;
-import java.util.List;
-import org.openbowl.common.AuthorizedUser;
-import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
-import java.io.OutputStream;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 import org.openbowl.common.BowlingGame;
-import org.openbowl.common.WebFunctions;
+import org.openbowl.common.CommonHandler;
 
 /**
  *
  * @author Open Bowl <http://www.openbowlscoring.org/>
  */
-public class BowlingGameDisplayHandler implements HttpHandler {
+public class BowlingGameDisplayHandler extends CommonHandler {
 
-    private final String SUCCESS = "success";
-    private final String ERROR_MSG = "error message";
     private final int lane;
-    private final Gson gson;
-    private final List<AuthorizedUser> users;
     private final BowlingGameDisplay game;
 
     public BowlingGameDisplayHandler(int lane, BowlingGameDisplay g) {
+        super();
         this.lane = lane;
-        gson = new Gson();
         this.game = g;
-        users = new ArrayList();
-        Calendar exp = Calendar.getInstance();
-        exp.add(Calendar.YEAR, 10);
-        users.add(new AuthorizedUser("yZ9Ut95MG3xdf5gc6WgT", exp));
-    }
-
-    public void addAuthorizedUser(AuthorizedUser u) {
-        users.add(u);
     }
 
     @Override
-    public void handle(HttpExchange he) throws IOException {
-        Map<String, List<String>> headers = he.getRequestHeaders();
-        String method = he.getRequestMethod();
-        if (isAuthorized(headers)) {
-            Map<String, String> parms = WebFunctions.queryToMap(he.getRequestURI().getQuery());
-            he.getResponseHeaders().set("Content-Type", "application/json");
-            OutputStream os = he.getResponseBody();
-            Map<String, Object> response = new HashMap<>();
-            switch (method) {
-                case WebFunctions.GET_METHOD: {
-                    response.putAll(onGet(parms));
-                    break;
-                }
-                case WebFunctions.POST_METHOD: {
-                    String body = new String(he.getRequestBody().readAllBytes(), StandardCharsets.UTF_8);
-                    response.putAll(onPost(parms, body));
-                    break;
-                }
-                default: {
-                    response.put(SUCCESS, false);
-                    response.put(ERROR_MSG, "unsupported method");
-                    break;
-                }
-
-            }
-
-            String jsonResponse = gson.toJson(response);
-            he.sendResponseHeaders(200, jsonResponse.length());
-            os.write(jsonResponse.getBytes());
-            os.close();
-
-        } else {
-            //sends 403 Forbidden
-            he.sendResponseHeaders(403, 0);
-            he.getResponseBody().close();
-        }
-    }
-
-    private Map<String, Object> onGet(Map<String, String> parms) {
+    protected Map<String, Object> onGet(Map<String, String> parms) {
         Map<String, Object> map = new HashMap<>();
         map.put("Lane", lane);
         switch (parms.getOrDefault("get", "none")) {
@@ -100,7 +39,8 @@ public class BowlingGameDisplayHandler implements HttpHandler {
         return map;
     }
 
-    private Map<String, Object> onPost(Map<String, String> parms, String body) {
+    @Override
+    protected Map<String, Object> onPost(Map<String, String> parms, String body) {
         Map<String, Object> map = new HashMap<>();
         map.put("Lane", lane);
         Map<String, Object> requestBody = new HashMap<>();
@@ -171,17 +111,4 @@ public class BowlingGameDisplayHandler implements HttpHandler {
         return map;
     }
 
-    private boolean isAuthorized(Map<String, List<String>> headers) {
-        Iterator<AuthorizedUser> itr = users.listIterator();
-        while (itr.hasNext()) {
-            AuthorizedUser u = itr.next();
-            if (u.isAuthorized(headers)) {
-                return true;
-            }
-            if (u.isExpired()) {
-                itr.remove();
-            }
-        }
-        return false;
-    }
 }
