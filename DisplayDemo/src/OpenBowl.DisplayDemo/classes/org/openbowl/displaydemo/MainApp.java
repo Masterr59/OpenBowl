37fef8446e181f3b5cb04b64a871cc202789f419
patrick <patrick@enburg.info>
2020-04-18 21:18:44 -0700

    Added Display Demo app

---
+++ DisplayDemo/src/OpenBowl.DisplayDemo/classes/org/openbowl/displaydemo/MainApp.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2020 Open Bowl <http://www.openbowlscoring.org/>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.openbowl.displaydemo;
+
+/**
+ *
+ * @author Open Bowl <http://www.openbowlscoring.org/>
+ */
+public class MainApp {
+    
+    public static void main(String[] args) {
+        if(args.length == 1){
+            System.out.printf("Display Address: %s\n", args[0]);
+            DemoGame game = new DemoGame(args[0]);
+            game.runDemo();
+        }
+        else{
+            System.out.println("Requires Display Address");
+        }
+    }
+    
+}
