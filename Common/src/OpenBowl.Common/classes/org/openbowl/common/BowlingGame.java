775c7fae006aa1a2b0170560aca22417a0c10432
Patrick Enburg <patrick@enburg.info>
2020-06-07 16:19:00 -0700

    Refactored bowling frame / game structure to add balls properly

--- Common/src/OpenBowl.Common/classes/org/openbowl/common/BowlingGame.java
+++ Common/src/OpenBowl.Common/classes/org/openbowl/common/BowlingGame.java
@@ -26,55 +26,49 @@ public class BowlingGame {
 
     private BowlingFrame[] mFrames;
     private int mFrameIndex;
+    private int mBallIndex;
     private String mPlayerName;
     private int mHandicap;
+    private int mTap;
     private boolean mFinishedFrame;
     private String mPlayerUUID;
+    private int mGameScore;
 
-    public BowlingGame(String playerName, int handicap, String playerUUID) {
+    public BowlingGame(String playerName, int handicap, String playerUUID, int tap) {
         mFrames = new BowlingFrame[10];
         mFrameIndex = 0;
+        mBallIndex = 0;
         mPlayerName = playerName;
         mHandicap = handicap;
+        mTap = tap;
         mFinishedFrame = false;
         mPlayerUUID = playerUUID;
+        mGameScore = 0;
     }
 
     public void bowled(ArrayList<BowlingPins> remainingPins, boolean foul, double speed) {
+        if (mFrameIndex < 9 && mBallIndex > 1) {
+            mFrameIndex++;
+        }
         if (mFrames[mFrameIndex] == null) {
             initFrame();
         }
+        mFrames[mFrameIndex].addBall(remainingPins, foul, speed);
+        if (isStrikeSpare(mFrames[mFrameIndex].getBallRawScore(mBallIndex))) {
+            if (mFrameIndex < 9 && mBallIndex == 0) {//strike
+                mFrameIndex++;
+            }
 
-        if (!mFrames[mFrameIndex].isFinished()) {
-            mFrames[mFrameIndex].addBall(remainingPins, foul, speed);
-            if (mFrames[mFrameIndex].isFinished()) {
-                mFinishedFrame = true;
-            }
-        } else {
-            mFrameIndex++;
-            if (!isFinished()) {
-                initFrame();
-                mFrames[mFrameIndex].addBall(remainingPins, foul, speed);
-            }
         }
+        mBallIndex++;
     }
 
     private void initFrame() {
-        if (mFrameIndex == mFrames.length - 1) {
-            mFrames[mFrameIndex] = new BowlingFrame(true);
-        } else {
-            mFrames[mFrameIndex] = new BowlingFrame(false);
-        }
+        mFrames[mFrameIndex] = new BowlingFrame();
+        mBallIndex = 0;
     }
 
     public boolean isFinished() {
-        if (mFrameIndex >= mFrames.length) {
-            return true;
-        } else if (mFrameIndex == mFrames.length - 1) {
-            if (mFrames[mFrameIndex] != null && mFrames[mFrameIndex].isFinished()) {
-                return true;
-            }
-        }
 
         return false;
     }
@@ -89,21 +83,12 @@ public class BowlingGame {
         handicap = mHandicap;
     }
 
-    public void setFinishedFrame(boolean finishedFrame) {
-        mFinishedFrame = finishedFrame;
-    }
-
     public void updateTo(BowlingGame bg) {
         mFrames = bg.getFrames();
         mFrameIndex = bg.getFrameIndex();
         mPlayerName = bg.getPlayerName();
         mHandicap = bg.getHandicap();
-        mFinishedFrame = bg.getFinishedFrame();
         mPlayerUUID = bg.getPlayerUUID();
-    }
-
-    public boolean getFinishedFrame() {
-        return mFinishedFrame;
     }
 
     public BowlingFrame[] getFrames() {
@@ -133,15 +118,53 @@ public class BowlingGame {
             if (mFrames[x] == null) {
                 result += "[  ]";
             } else {
-                result += mFrames[x].toString();
+                result += "[";
+                result += getBallStringValue(x, 0);
+                result += " ";
+                result += getBallStringValue(x, 1);
+                if (x == 9) {
+                    result += " ";
+                    result += getBallStringValue(x, 2);
+                }
+                result += "]";
             }
         }
-        result += "handicap: " + mHandicap + " Score: (tbd)";
+        result += "handicap: " + mHandicap + " Score: " + mGameScore;
 
         return result;
     }
 
     public int getGameScore() {
-        return 0;
+        return this.mGameScore;
+    }
+
+    public String getBallStringValue(int frame, int ball) {
+        String ret = " ";
+        if (frame < 10 && mFrames[frame] != null && ball < 3) {
+            int ball1 = mFrames[frame].getBallRawScore(0);
+            int ball2 = mFrames[frame].getBallRawScore(1);
+            int ball2Diff = ball2 - ball1;
+            int ball3 = mFrames[frame].getBallRawScore(2);
+            int ball3Diff = ball3 - ball1;
+
+            if (ball == 0) {
+                ret = isStrikeSpare(ball1) ? "X" : Integer.toString(ball1);
+            } else if (ball == 1) {
+                ret = isStrikeSpare(ball2) ? "/" : Integer.toString(ball2Diff);
+                ret = (isStrikeSpare(ball1) && frame < 9) ? "#" : ret;
+                ret = (isStrikeSpare(ball2) && frame == 9) ? "X" : ret;
+                ret = ball1 == ball2 && !isStrikeSpare(ball1) ? "-" : ret;
+            } else if (ball == 2) {
+                ret = isStrikeSpare(ball3) ? "X" : Integer.toString(ball3Diff);
+                ret = isStrikeSpare(ball3) && !isStrikeSpare(ball2) ? "/" : ret;
+                ret = (ball2 == ball3 && !isStrikeSpare(ball2)) ? "-" : ret;
+            }
+            ret = mFrames[frame].isFoul(ball) ? "F" : ret;
+        }
+        return ret;
+    }
+
+    private boolean isStrikeSpare(int rawScore) {
+        return rawScore >= mTap;
     }
 }
